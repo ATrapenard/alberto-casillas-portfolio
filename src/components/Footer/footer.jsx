dbd2d851c1220a54377915c663c0f18e14782073
Alan Trapenard <alan.trapenard@gmail.com>
2024-10-19 13:46:04 -0700

    finalized and styled footer component

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -7,19 +7,41 @@ const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        <p>
-          Alberto Casillas |{" "}
-          <a href="mailto:albertocasilla123@gmail.com">
-            albertocasilla123@gmail.com
+        <div className="footer-left">
+          <p>
+            Alberto Casillas |{" "}
+            <a href="mailto:albertocasilla123@gmail.com">
+              albertocasilla123@gmail.com
+            </a>{" "}
+            |
+          </p>
+          <a
+            href="https://www.linkedin.com/in/-alberto-casillas/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedinIn className="icon" />
           </a>
-        </p>
-        <a
-          href="https://www.linkedin.com/in/-alberto-casillas/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaLinkedinIn className="icon" />
-        </a>
+        </div>
+        <div className="footer-center">
+          <p>&copy;Alberto Casillas. All rights reserved.</p>
+        </div>
+        <div className="footer-right">
+          <ul className="footer-links">
+            <li className="link">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="link">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="link">
+              <Link to="/skills">Skills</Link>
+            </li>
+            <li className="link">
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
